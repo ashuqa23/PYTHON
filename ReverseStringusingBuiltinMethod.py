3f61b918040dc664aba5cea7e73b84d88f66fb62
ashuqa23 <ashuqa23@gmail.com>
2026-02-19 07:15:58 +0530

    new program

---
+++ ReverseStringusingBuiltinMethod.py
@@ -0,0 +1,6 @@
+# Taking input from user
+string = input("Enter a string:")
+# Using builtin reverse method
+reversed_string = "".join(reversed(string))
+# Print the result
+print("Reversed string: ", reversed_string)
